2f1f5adce0b017928a30459cacc86f2b5d941482
Sem Peters <sem.peters@hotmail.com>
2023-12-12 18:08:53 +0100

    [#2] Add a radiobuttongroup for controlling image output: png, webp or jpeg

--- src/lib/Form.svelte
+++ src/lib/Form.svelte
@@ -14,5 +14,33 @@
     <input type="range" min="1" max="40" bind:value={formData.options.scale} />
   </label>
 
+  <fieldset>
+    <legend>File extension</legend>
+    <label>
+      <input
+        type="radio"
+        value="image/png"
+        bind:group={formData.options.type}
+      />
+      PNG
+    </label>
 
+    <label>
+      <input
+        type="radio"
+        value="image/jpeg"
+        bind:group={formData.options.type}
+      />
+      JPEG
+    </label>
+
+    <label>
+      <input
+        type="radio"
+        value="image/webp"
+        bind:group={formData.options.type}
+      />
+      WEBP
+    </label>
+  </fieldset>
 </form>
